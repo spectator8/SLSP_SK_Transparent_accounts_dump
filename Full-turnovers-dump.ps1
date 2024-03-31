9c106c24f1770e89fc23add067e1049a7cb30894
Ján Kamodi <jan.kamodi@solera.com>
2024-03-31 20:25:29 +0200

    aaaaaaaaaaaaaaaaaa

--- Full-turnovers-dump.ps1
+++ Full-turnovers-dump.ps1
@@ -1,4 +1,4 @@
-#acc get
+﻿#acc get
 
 $llf="fakepath\acc_list.txt"
 Clear-Content $llf
@@ -10,7 +10,8 @@ $link="https://api.slsp.sk/ta-accounts/api/accounts?size=$size"
 $accGetRes=""
 $accGetRes=irm $link
 
-
+$ibanNamePF="fakepath\iban-name-mapping.properties"
+Clear-Content $ibanNamePF
 
 
 Write-Host "acc count is: " $accGetRes.accounts.Count
@@ -24,6 +25,17 @@ $rrx=$rr.name + "|" + $rr.created + "|" + $rr.iban +"|" + $rr.balance.value + "|
 
 
 Out-File -FilePath $llf -InputObject $rrx -Width 99999 -Append
+
+#acc get
+
+#iban-name mapping file
+
+
+$ibanMap=""
+$ibanMap=$rr.iban +"="+ $rr.name
+Out-File -FilePath $ibanNamePF -InputObject $ibanMap -Width 99999 -Append
+
+#iban-name mapping file
 
 }
 
@@ -61,9 +73,17 @@ foreach ($to in $fxres.turnovers)
 
 {
 
+$ibb=""
+$ibb=$to.accountIban
+$hhh=""
+[string]$hhh=(Get-Content $ibanNamePF | Select-String $ibb)
+
+$nameX=""
+$nameX=$hhh.Substring($hhh.IndexOf("=")+1)
+
 
 $full=""
-$full=$to.accountIban + "|" + $to.turnoverId + "|" + $to.counterAccountName + "|" + $to.date + "|" + $to.amount.value + "|" + $to.amount.currency + "|" + $to.note + "|" + $to.description
+$full=$nameX + "|" + $to.accountIban + "|" + $to.turnoverId + "|" + $to.counterAccountName + "|" + $to.date + "|" + $to.amount.value + "|" + $to.amount.currency + "|" + $to.note + "|" + $to.description
 
 Out-File -FilePath $llff -InputObject $full -Width 99999 -Append
 
